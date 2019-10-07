d8a28e28467959b8b27dd6929c38ceb1b6a905ab
Georgi Bojinov <georgi.bojinov@hotmail.com>
2019-10-07 08:49:27 +0300

    [#6] Add config TOML parser with tomland library (#9)
    * Add config TOML parser with tomland library
    
    * Apply comments

--- src/Crocodealer/Config.hs
+++ src/Crocodealer/Config.hs
@@ -1,18 +1,40 @@
+{- | Configurations through the @config.toml@ file@.
+-}
+
 module Crocodealer.Config
        ( Repo (..)
        , Config (..)
+       , loadConfig
        ) where
+
+import Toml (TomlCodec, (.=))
+import qualified Toml
 
 import Crocodealer.Core.Label as Label
 
-
 newtype Repo = Repo
     { unRepo :: Text
-    }
+    } deriving (Show)
+
+-- | TOML Codec for a list of 'Repo'
+repoCodec :: Toml.Key -> TomlCodec [Repo]
+repoCodec key = Toml.diwrap (Toml.arrayOf Toml._Text key)
 
 data Config = Config
     { configUsername            :: !(Maybe Text)
     , configRepository          :: !(Maybe Text)
     , configLabelRules          :: ![Label.LabelRule]
     , configIgnoredRepositories :: ![Repo]
-    }
+    } deriving (Show)
+
+-- | TOML Codec for the 'Config' data type.
+configCodec :: TomlCodec Config
+configCodec = Config
+    <$> Toml.dioptional (Toml.text "username") .= configUsername
+    <*> Toml.dioptional (Toml.text "template") .= configRepository
+    <*> Toml.list Label.labelRuleCodec "labelRule" .= configLabelRules
+    <*> repoCodec "ignoredRepositories" .= configIgnoredRepositories
+
+-- | Loads the @config.toml@ file.
+loadConfig :: MonadIO m => m Config
+loadConfig = Toml.decodeFile configCodec "config.toml"
--- src/Crocodealer/Core/Label.hs
+++ src/Crocodealer/Core/Label.hs
@@ -3,16 +3,41 @@ module Crocodealer.Core.Label
        , Label (..)
        , Name (..)
        , HexColor (..)
+       , labelRuleCodec
        ) where
 
+import Toml (Codec (..), Key, TomlCodec, (.=))
+import qualified Toml
+
+-- TODO remove this from here when it's part of tomland library
+disum
+    :: (Functor r, Alternative w)
+    => (c -> Maybe d) -- ^ Mapper for consumer
+    -> (a -> b)       -- ^ Mapper for producer
+    -> Codec r w d a  -- ^ Source 'Codec' object
+    -> Codec r w c b  -- ^ Target 'Codec' object
+disum match ctor codec = Codec
+    { codecRead = ctor <$> codecRead codec
+    , codecWrite = \c -> case match c of
+        Nothing -> empty
+        Just d  -> ctor <$> codecWrite codec d
+    }
 
 newtype Name a = Name
     { unName :: Text
     } deriving (Show)
 
+-- | TOML Codec for the 'Name' data type.
+nameCodec :: Key -> TomlCodec (Name a)
+nameCodec name = Toml.diwrap $ Toml.text name
+
+-- | TOML Codec for the 'HexColor' data type.
 newtype HexColor = HexColor
     { unHexColor :: Text
     } deriving (Show)
+
+hexColorCodec :: TomlCodec HexColor
+hexColorCodec = Toml.dimap unHexColor HexColor $ Toml.text "color"
 
 data Label = Label
     { labelName        :: !(Name Label)
@@ -20,11 +45,37 @@ data Label = Label
     , labelColor       :: !HexColor
     } deriving (Show)
 
+-- | TOML Codec for the 'Label' data type.
+labelCodec :: TomlCodec Label
+labelCodec = Label
+    <$> nameCodec "name" .= labelName
+    <*> Toml.text "description" .= labelDescription
+    <*> hexColorCodec .= labelColor
+
 -- | Create a Label or Override an existing one
 data LabelRule
     = Create !Label -- | Create a Label from scratch.
-    | Override     -- | Override an existing label. 
+    | Override     -- | Override an existing label.
     !(Name Label)   -- | First parameter is the existing label to be overridden.
     !(Name Label)   -- | Second parameter is the new label value.
     deriving (Show)
-              
+
+-- | TOML Codec for overriding labels
+namesCodec :: TomlCodec (Name Label, Name Label)
+namesCodec = (,)
+     <$> nameCodec "old" .= fst
+     <*> nameCodec "new" .= snd
+
+matchCreate :: LabelRule -> Maybe Label
+matchCreate (Create label) = Just label
+matchCreate _              = Nothing
+
+matchOverride :: LabelRule -> Maybe (Name Label, Name Label)
+matchOverride (Override old new) = Just (old, new)
+matchOverride _                  = Nothing
+
+-- | TOML Codec for the 'LabelRule' data type.
+labelRuleCodec :: TomlCodec LabelRule
+labelRuleCodec =
+    disum matchCreate Create (Toml.table labelCodec "create")
+    <|> disum matchOverride (uncurry Override) (Toml.table namesCodec "override")
